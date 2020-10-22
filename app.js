fb3fac5dc2a253d428c446b749296ff149378b23
Guilherme Cima <guilherme.cimabatista@gmail.com>
2020-10-22 15:40:43 +0200

    redo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ button.appendChild(content);
 spoiler.parentElement.replaceChild(button, spoiler);
 button.addEventListener('click', function () {
     button.parentElement.replaceChild(spoiler, button);
-});*/
+});
 
 let spoilers = document.querySelectorAll('.spoiler');
 for (let i = 0; i < spoilers.length; i++) {
@@ -19,4 +19,41 @@ for (let i = 0; i < spoilers.length; i++) {
         spoiler.innerHTML = content;
         spoiler.classList.remove('button-mode');
     })
-}
+}*/
+
+//Lets try something else
+
+let button = document.querySelector('.button');
+let spoilers = document.querySelectorAll('.spoiler');
+let hidden = true;
+button.addEventListener('click', function () {
+
+    if (hidden) {
+
+        for (let i = 0; i < spoilers.length; i++) {
+            const spoiler = spoilers[i];
+            spoiler.style.display = "inline-block";
+            setTimeout(function () {
+                spoiler.style.opacity = "1";
+            }, 20)
+        }
+        button.innerHTML = 'Hide spoilers';
+        hidden = false;
+
+    } else {
+
+        for (let i = 0; i < spoilers.length; i++) {
+            const spoiler = spoilers[i];
+            spoiler.style.opacity = "0";
+            setTimeout(function () {
+                spoiler.style.display = "none";
+            }, 500)
+        }
+        button.innerHTML = 'Show spoilers';
+
+        hidden = true;
+
+    }
+
+
+})
